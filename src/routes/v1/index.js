4fc1176feda3e1150a4f18fa679481393db70c89
NiHold <holder.nikolai@gmail.com>
2025-02-14 13:24:32 +0100

    added route bundler and imported it in app.js

---
+++ src/routes/v1/index.js
@@ -0,0 +1,14 @@
+// routes/index.js
+import { Router } from "express";
+import authRoutes from "../../api/auth/routes/auth.routes.js";
+import organisationRoutes from "../../api/organisations/routes/organisation.routes.js";
+import invitationRoutes from "../../api/invitations/routes/invitation.routes.js";
+
+const router = Router();
+
+// Keep your existing structure
+router.use("/auth", authRoutes);
+router.use("/organisations", organisationRoutes);
+router.use("/", invitationRoutes);
+
+export default router;
